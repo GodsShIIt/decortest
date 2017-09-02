02a5fd5891c8562e59bd203c86c4e3a60333052b
GodsShIIt <builder112233@gmail.com>
2017-09-02 21:00:31 +0400

    Decortest

---
+++ version.lua
@@ -0,0 +1,21 @@
+---------------Version 4.1
+----Added blocks
+--+ Cement
+--+ Wet Cement
+--+ Concrete
+--+ Colored concrete
+--+ Masonry
+--+ Armature
+----Added Items
+--+ Cement
+----Added craft
+--+ Cement
+--+ Concrete
+--+ Colored concrete
+--+ Colored concrete x8
+--+ Masonry
+--+ Armature
+----Added features
+--+ Water washes paint off concrete
+--+ The cement becomes wet next to water
+
